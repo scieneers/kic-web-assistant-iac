3af27db1f59cf458b2c9a5d5f2e8f75cb06a8419
Jonas Bremer <jonas.bremer@scieneers.de>
2024-07-16 18:00:41 +0200

    webapp rest api

---
+++ tf/dev/restapi.tf
@@ -0,0 +1,30 @@
+resource "azurerm_service_plan" "restapi" {
+  name                = "${local.resource_prefix}-restapi-service-plan-${local.environment}"
+  resource_group_name      = azurerm_resource_group.kic_web_assistant_rg.name
+  location                 = azurerm_resource_group.kic_web_assistant_rg.location
+  os_type             = "Linux"
+  sku_name            = "B2"
+}
+
+resource "azurerm_linux_web_app" "restapi" {
+  name                = "kic-restapi-${local.environment}"
+  resource_group_name = azurerm_resource_group.kic_web_assistant_rg.name
+  location            = azurerm_resource_group.kic_web_assistant_rg.location
+  service_plan_id     = azurerm_service_plan.streamlit_service_plan.id
+
+  site_config {
+    application_stack {
+      docker_image_name   = "rest-api:latest"
+      docker_registry_url = "https://${azurerm_container_registry.kic_assistant.login_server}"
+    }
+    container_registry_use_managed_identity = true
+    container_registry_managed_identity_client_id = azurerm_user_assigned_identity.uai.client_id
+  }
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.uai.id]
+  }
+
+  https_only = true
+}
